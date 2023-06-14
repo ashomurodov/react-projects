c88bd78ff736646634ea1f92cf60499691ea589c
Ayyub <ayyuubshomurodov@gmail.com>
2023-06-14 10:33:05 +0500

    added

---
+++ src/TodoList.js
@@ -0,0 +1,10 @@
+import TodoForm from "./TodoForm";
+
+export default function TodoList() {
+	return (
+		<div className="container">
+			<h1>Welcome to home!</h1>
+			<TodoForm />
+		</div>
+	);
+}
