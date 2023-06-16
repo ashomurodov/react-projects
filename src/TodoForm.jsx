5e3a62d5e8f083beba3f12b654ff50d31ab7ab65
Ayyub <ayyuubshomurodov@gmail.com>
2023-06-16 11:02:16 +0500

    feat

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,35 +1,43 @@
-import { useState } from "react";
+import React from "react";
 
-export default function TodoForm() {
-	const [input, setInput] = useState("");
-	const [list, setList] = useState([]);
+export class TodoForm extends React.Component {
+	// const [input, setInput] = useState("");
+	// const [list, setList] = useState([]);
 
-	const handleSubmit = (e) => {
+	// const handleSubmit = (e) => {
+	// 	e.preventDefault();
+
+	// 	(function setListFunc() {
+	// 		const newList = {
+	// 			id: Math.floor(Math.random() * 1000),
+	// 			todo: input,
+	// 		};
+
+	// 		setList([...list, newList]);
+	// 		console.log([newList, ...list]);
+	// 	})();
+	// };
+
+	state = {};
+	inputRef = React.createRef();
+
+	handleSubmit = (e) => {
 		e.preventDefault();
+		const newTodo = {
+			id: Math.random() * 1000,
+			todo: this.inputRef.current.value,
+		};
 
-		(function setListFunc() {
-			const newList = {
-				id: Math.floor(Math.random() * 1000),
-				todo: input,
-			};
-
-			setList([...list, newList]);
-			console.log([newList, ...list]);
-		})();
+		this.setState((prevObj) => ({ ...prevObj, newTodo }));
+		console.log(this.state);
 	};
 
-	return (
-		<form className="container" onSubmit={handleSubmit}>
-			<input
-				type="text"
-				value={input}
-				onChange={(e) => {
-					setInput(e.target.value);
-					console.log(e.target.value);
-				}}
-				className="todo-input"
-			/>
-			<button className="todo-btn">submit</button>
-		</form>
-	);
+	render() {
+		return (
+			<form className="container" onSubmit={this.handleSubmit}>
+				<input ref={this.inputRef} type="text" className="todo-input" />
+				<button className="todo-btn">submit</button>
+			</form>
+		);
+	}
 }
