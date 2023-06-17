6a46257644a65586878eb2d54bee52bad9d56fe8
Ayyub <ayyuubshomurodov@gmail.com>
2023-06-17 13:31:24 +0500

    added ts files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { TodoList } from "./TodoList";
 
 export class App extends React.Component {
 	render() {
-		return <TodoList />;
+		let isTrue: boolean = false;
+		isTrue = 4;
+		console.log(isTrue);
+
+		return <h1>Hello World</h1>;
 	}
 }
